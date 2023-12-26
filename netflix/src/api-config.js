a8bdf9a2d998267375ba708f5c8fa783d870e8c6
Divyanshu Sahu <55633712+d2207-sahu@users.noreply.github.com>
2023-12-26 16:35:51 +0530

    api-key

---
+++ netflix/src/api-config.js
@@ -0,0 +1,7 @@
+const options = {
+    method: 'GET',
+    headers: {
+      accept: 'application/json',
+      Authorization: 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJkZDljNmM5MDNhOGNhYmZiZDg2ODgxNDljYmQ5NmY3NCIsInN1YiI6IjY1OGE3ZTQ0NzY0NmZkNGVlMTA1NmE3YyIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.yv_O8mCvOp9kLixRhnDSBDvMLrdbY_3osbbYzwRBVeE'
+    }
+  };
