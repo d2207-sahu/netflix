218005cafbfc2dad29a238dcee820c62f188e2f6
= <=>
2024-01-04 02:38:51 +0530

    fixed-video-overlay

--- netflix/src/components/globals/Logo.jsx
+++ netflix/src/components/globals/Logo.jsx
@@ -1,10 +1,13 @@
 import React from "react";
 import styled, { css } from "styled-components";
 import { background_logo_large } from '../../assets';
+import { useLocation, useNavigate } from "react-router-dom";
+import { routingConfig } from "../../router/routing-config";
 
 const LogoImageComponent = styled.img`
   width: 20rem;
   margin-left: 1.5%;
+  cursor: pointer;
   @media only screen and (max-width: 767px) {
         /* Mobile styles */
         ${({ $mobileStyles }) => $mobileStyles && css`${$mobileStyles}`}
@@ -17,7 +20,13 @@ const LogoImageComponent = styled.img`
 `;
 
 const Logo = () => {
+  const { pathname } = useLocation();
+  const { navigate } = useNavigate()
   return <LogoImageComponent
+    onClick={() => {
+      if (!(pathname === routingConfig.home))
+        navigate(routingConfig.home)
+    }}
     src={background_logo_large}
     alt="Netflix Logo"
   />;
